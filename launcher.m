7b6fe2a728f023d47fcdcd665072c1cf61d94343
Miyu <camillemaurice@gmail.com>
2015-04-25 18:14:36 +0200

    exp + theory centroids

--- launcher.m
+++ launcher.m
@@ -4,7 +4,9 @@ clc;
 %Open the image
 C_image = imread('phanton_no_noise.tif');
 BW = im2bw(C_image, 0.35);
-%imshow(BW)
+%imshow(medfilt2(BW)
+%BW =medfilt2(BW); (for the noisy phantom)
+
 %Work quite good but pb on the borders...Maybe we should ignore the
 %borders? 
 s = regionprops(BW, 'Centroid');
@@ -13,36 +15,45 @@ exp_centroids = cat(1, s.Centroid);
 %hold on
 %plot(exp_centroids(:,1),exp_centroids(:,2), 'b*')
 %hold off
-exp_centroids;
+
 %Open the windowed version (find how to get it automatically)
 W_image = imread('phanton_t2.tif');
 
 %Compute FT of both
-ft_C_image = fft2(C_image);
+ft_C_image = fft2(BW);
 ft_W_image = fft2(W_image);
 
 %%Then correlation image to find the centroids
 Correlation = ft_C_image.*ft_W_image;
 R = ifft2(Correlation);
+
 %find the max and only keep the max. It correspond to the value of the
 %pixels that are true center
 M = max(max(R));
 R(R<M) = 255;
 R(R>=M) = 0;
 
+%imshow(R)
 [row,col] = find(R<255);
 theory_centroids = cat(2,row,col);
 
+%sort the coordinates in x ascending order
+[x,y] = sort(theory_centroids(:,1));
+sorted = theory_centroids(y,:);
+
+theory_centroids = sorted;
+exp_centroids;
+
+
 %Problem: I found the same coordinates for the theoretical ones and the
 %experimental ones ! (except on the borders)
-imshow(BW)
+imshow(C_image)
 hold on
 plot(theory_centroids(:,1),theory_centroids(:,2), 'b*')
 plot(exp_centroids(:,1),exp_centroids(:,2), 'ro')
 hold off
 
-%size(theory_centroids)
-%size(exp_centroids)
+
 
 %Other method, use filter2
 %We should get the ideal centroids ?
