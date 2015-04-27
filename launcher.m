b46f8a329d6cabab8ce4ad5182ea77c66a84e624
charlotteassemat <charlotteassemat33@gmail.com>
2015-04-27 21:36:00 +0200

    note and modif about centroids.

--- launcher.m
+++ launcher.m
@@ -1,11 +1,14 @@
+%right now i strongly think it doesn't work only cause i haven't figured out borders yet
+% will see that after bayesian
+
 clear all;
 clc;
 
 %Open the image
 C_image = imread('phanton_no_noise.tif');
 BW = im2bw(C_image, 0.35);
-%figure()
-%imshow(BW)
+figure(1)
+imshow(BW)
 
 
 %Get border im, see if it help
@@ -28,28 +31,39 @@ BW_B(1:32,:) = 0;
 BW_B(:,Y-32:Y) = 0;
 BW_B(X-32:X,:) = 0;
 
-s = regionprops(BW, 'Centroid');
+%s = regionprops(BW, 'Centroid'); %old line
+s = regionprops(BW_B, 'Centroid'); % i guess that's what you wanted to do? or where do you use BW_B?
 exp_centroids = cat(1, s.Centroid);
+
 
 %Works quite well, but problem on the borders, I dont think the center is
 %well found. Uncomment the figure to check it visually
-%figure()
-%imshow(BW)
+%i think it works no?
+%figure(1)
+%imshow(BW_B)
 %hold on
 %plot(exp_centroids(:,1),exp_centroids(:,2), 'b*')
 %hold off
+
+%Attemps to compute the windowed version, still a work in progress
+%CC = bwconncomp(BW_B);
+%N = sqrt(CC.NumObjects);
+%assert(mod(N,1)==0);
+%assert(mod(N,2)==1);%check it's integer and odd number
+%N
 
 %Open the windowed version (find how to get it automatically)
 W_image = imread('phanton_t2.tif');
 
 %Compute FT of both
-ft_C_image = fft2(BW);
+ft_C_image = fft2(BW_B);%shouldn't we use BW_B?
 ft_W_image = fft2(W_image);
 
 %%Then correlation image to find the centroids
-Correlation = ft_C_image.*ft_W_image;
+Correlation = ft_C_image.*ft_W_image;%xcorr(ft_C_image(:,:),ft_W_image(:,:));%f
 R = ifft2(Correlation);
-%imshow(R)
+figure(2)
+imshow(R)
 
 
 %find theoretical centroids -- method 2
@@ -59,12 +73,19 @@ R_WB = imcomplement(R_BW);
 t = regionprops(R_WB, 'Centroid');
 th_centroids = cat(1, t.Centroid);
 
-figure()
+figure(3)
 imshow(R_BW)
+figure(4)
 hold on
 plot(th_centroids(:,1),th_centroids(:,2), 'ro')
 hold off
 
+% about the number of centroids:
+% i think we should detect 15*15=225 centroids
+% what you call number of centroids expected is actually the number of
+% detected objects in the picture, ie it includes all the half-circles that
+% we don't wan't to detect, 289=15*15+(15+2)*4-4
+% i guess we only get 256
 S1 =['nombre de centroids theoriques : ',num2str(size(th_centroids,1))];
 display(S1)
 S2 = ['nombre de centroids exp : ',num2str(size(exp_centroids,1))];
@@ -72,7 +93,7 @@ display(S2)
 
 %Less centroids in the theoretical result (problem with the borders maybe
 %?)
-figure()
+figure(5)
 imshow(C_image);
 hold on
 plot(th_centroids(:,1),th_centroids(:,2), 'ro')
@@ -81,6 +102,7 @@ hold off
 
 %We should detect the same number of centroids
 %Perform difference of the coordinates to get displacement
+
 
 
 %Interpolation methods : http://www.mathworks.com/help/vision/ug/interpolation-methods.html
