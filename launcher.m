44483a6ca11563e200aba4ef79ff2675a4e32731
charlotteassemat <charlotteassemat33@gmail.com>
2015-05-05 12:13:17 +0200

    *correct distance interpolation working, *with grayscale image, *reconstructed image written in image file, *displaying original orig/computed and final/orig centroids

--- launcher.m
+++ launcher.m
@@ -1,6 +1,4 @@
-%right now i strongly think it doesn't work only because we should crop the
-%image and not have borders, will take care of it tonight or in the morning
-
+close all;
 clear all;
 clc;
 
@@ -17,22 +15,10 @@ size_phantom = 64;
 %centroids
 rect = [size_phantom/2, size_phantom/2, X-size_phantom, Y-size_phantom];
 BW_B = imcrop (BW,rect);
-
+no_B=imcrop(C_image,rect);
+imwrite(no_B,'phanton_recNoB.gif');
 s = regionprops(BW_B, 'Centroid'); 
 exp_centroids = cat(1, s.Centroid);
-
-%figure(1)
-%imshow(BW_B)
-%hold on
-%plot(exp_centroids(:,1),exp_centroids(:,2), 'b*')
-%hold off
-
-%Attemps to compute the windowed version, still a work in progress
-%CC = bwconncomp(BW_B);
-%N = sqrt(CC.NumObjects);
-%assert(mod(N,1)==0);
-%assert(mod(N,2)==1);%check it's integer and odd number
-%N
 
 %Open the windowed version (find how to get it automatically)
 %and crop it so they both have same size
@@ -57,68 +43,76 @@ R_WB = imcomplement(R_BW);
 t = regionprops(R_WB, 'Centroid');
 th_centroids = cat(1, t.Centroid);
 
-%figure(3)
-%imshow(R_BW)
-%hold on
-%plot(th_centroids(:,1),th_centroids(:,2), 'ro')
-%hold off
-
 S1 =['nombre de centroids theoriques : ',num2str(size(th_centroids,1))];
 display(S1)
 S2 = ['nombre de centroids exp : ',num2str(size(exp_centroids,1))];
 display(S2)
 
-
-figure(5)
+imwrite(BW_B,'no_borders.tif');
+figure(1)
 imshow(BW_B);
 hold on
 plot(th_centroids(:,1),th_centroids(:,2), 'ro')
 plot(exp_centroids(:,1),exp_centroids(:,2), 'b*')
 hold off
 
+
 %Perform difference of the coordinates to get displacement
 Vectors = th_centroids - exp_centroids;
+N=sqrt(size(th_centroids,1));
+dist=64;
+[X_c Y_c]=size(no_B);
+comp_image=zeros(size(no_B));
+for i=1:X_c
+    for j=1:Y_c
+        P=[i j];
+        pts_around=zeros(4,2);%ul ur lr ll
+        %spans from 0 to 15
+        pts_around(1,:) = [floor(i/dist) floor(j/dist)];
+        pts_around(2,:) = [floor(i/dist) ceil(j/dist)];
+        pts_around(3,:) = [ceil(i/dist) ceil(j/dist)];
+        pts_around(4,:) = [ceil(i/dist) floor(j/dist)];
+    
+        if(0==0)%sum(pts_around,2)>0)%if for each pt x+ y>0
+            offset=zeros(4,2);
+            if(sum(pts_around,2)>0)
+            for it=1:4
+               offset(it,:) = Vectors(pts_around(it,1)*N + pts_around(it,2));
+            end
+            end
+            new_pos = [pts_around(:,1)*dist+offset(:,1) pts_around(:,2)*dist+offset(:,2)];
+            new_pos(new_pos>X_c)=X_c;
+            %if 2 or just 1dim =0 then 0
+            dist_pts=norm([P; P; P; P]-pts_around);
+            if (sum(dist_pts==0)>1)%we are on a interpolation point so just assign
+                display('on a point')
+                new_pst = new_pos(dist_pts==0);
+            else
+                %interpolation here we go!
+                new_pst(1) = i + 1/sum(1./(dist_pts))*sum((offset(:,1))./dist_pts);
+                new_pst(2) = j + 1/sum(1./(dist_pts))*sum((offset(:,2))./dist_pts);
+                
+            end  
+            new_pst=floor(new_pst);
+            new_pst(new_pst<0)=0;
+            new_pst(new_pst>Y_c)=Y_c;
+            comp_image(new_pst(1),new_pst(2)) = no_B(i,j);
+        else%if 1 around point equals 0 then 0
+            comp_image(i,j) = 0;
+       end
+    end
+end
+sum(unique(comp_image)~=unique(no_B))
+imwrite(comp_image,'phanton_reconstructed.gif');
+recBW = im2bw (comp_image, 0.35);
+recWB = imcomplement(recBW);
+t2 = regionprops(recBW, 'Centroid');
+fin_centroids = cat(1, t2.Centroid);
+figure(2)
+imshow(comp_image);
+hold on
+plot(fin_centroids(:,1),fin_centroids(:,2), 'ro')
+plot(exp_centroids(:,1),exp_centroids(:,2), 'g*')
 
+hold off
 
-
-
-
-
-
-
-
-
-%Interpolation methods : http://www.mathworks.com/help/vision/ug/interpolation-methods.html
-%
-%Other method, use filter2
-%We should get the ideal centroids ?
-%Correlation = filter2(W_image,C_image,'same');
-%Correlation(Correlation>255) = 255;
-%figure()
-%imshow(Correlation)
-
-
-%find centroids -- method 1
-%find the max and only keep the max. It correspond to the value of the
-%pixels that are true center
-%M = max(max(R));
-%R(R<M) = 255;
-%R(R>=M) = 0;
-%imshow(R)
-%[row,col] = find(R<255);
-%theory_centroids2 = cat(2,row,col);
-%sort the coordinates in x ascending order
-% [x,y] = sort(theory_centroids2(:,1));
-% sorted = theory_centroids2(y,:);
-% 
-% theory_centroids2 = sorted;
-%M = max(max(R));
-%R(R<M) = 255;
-%R(R>=M) = 0;
-%imshow(R)
-%[row,col] = find(R<255);
-%theory_centroids2 = cat(2,row,col);
-%sort the coordinates in x ascending order
-%[x,y] = sort(theory_centroids2(:,1));
-%sorted = theory_centroids2(y,:);
- 
